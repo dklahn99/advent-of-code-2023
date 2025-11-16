0cd31c2ab65f5933da91431e260b4f7c79d21007
Daniel Klahn <dklahn@mit.edu>
2025-11-16 11:30:51 -0500

    Day 8 part 1: rewrite to use string slices

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,12 +1,30 @@
 use std::collections::HashMap;
 use std::fs;
 
-fn parse_line(line: &str) -> (String, String, String) {
-    let node = line[0..3].to_string();
-    let left = line[7..10].to_string();
-    let right = line[12..15].to_string();
+fn parse_line(line: &str) -> (&str, &str, &str) {
+    let node = &line[0..3];
+    let left = &line[7..10];
+    let right = &line[12..15];
 
     return (node, left, right);
+}
+
+fn part1(sequence: &str, map: &HashMap<&str, (&str, &str)>) {
+    let mut sequence_index: usize = 0;
+    let mut current_node: &str = "AAA";
+    let mut num_steps: u32 = 0;
+    while current_node != "ZZZ" {
+        if sequence.chars().nth(sequence_index).expect("OOB") == 'R' {
+            current_node = map[&current_node].1;
+        } else {
+            current_node = map[&current_node].0;
+        }
+
+        sequence_index = (sequence_index + 1) % sequence.chars().count();
+        num_steps += 1;
+    }
+
+    println!("Part 1: Num Steps: {}", num_steps);
 }
 
 fn main() {
@@ -15,25 +33,11 @@ fn main() {
 
     let sequence: &str = lines[0];
 
-    let mut map: HashMap<String, (String, String)> = HashMap::new();
+    let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
     for line in &lines[2..] {
         let (node, left, right) = parse_line(line);
         map.insert(node, (left, right));
     }
 
-    let mut sequence_index: usize = 0;
-    let mut current_node: String = String::from("AAA");
-    let mut num_steps: u32 = 0;
-    while current_node != "ZZZ" {
-        if sequence.chars().nth(sequence_index).expect("OOB") == 'R' {
-            current_node = map[&current_node].1.clone();
-        } else {
-            current_node = map[&current_node].0.clone();
-        }
-
-        sequence_index = (sequence_index + 1) % sequence.chars().count();
-        num_steps += 1;
-    }
-
-    println!("Part 1: Num Steps: {}", num_steps);
+    part1(sequence, &map);
 }
